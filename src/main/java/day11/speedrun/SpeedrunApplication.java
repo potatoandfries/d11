996c881527d28c90fb17319b688c2b1a1283abb0
boris <BORI0001@e.ntu.edu.sg>
2023-12-06 23:12:10 +0800

    lets go lets go

---
+++ src/main/java/day11/speedrun/SpeedrunApplication.java
@@ -0,0 +1,13 @@
+package day11.speedrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedrunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/day11/speedrun/SpeedrunApplicationTests.java
@@ -0,0 +1,13 @@
+package day11.speedrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
